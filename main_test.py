2a6301013d09088ab9afaca2dabc2496abbf7872
AeTen07 <w101882048@gmail.com>
2026-04-20 22:56:11 +0800

    Update main_test.py

--- main_test.py
+++ main_test.py
@@ -1,30 +1,8 @@
-import streamlit as st
-from sidebar import render_sidebar
-from page_modules.home_page import render_home_page
-from page_modules.search_page import render_search_page
-from page_modules.analysis_page import render_analysis_page
+import google.generativeai as genai
 
-def main():
-#主應用程式入口
+genai.configure(api_key="你的API KEY")
 
-    st.set_page_config(layout="wide")
-    
-    # 初始化頁面狀態
-    if 'current_page' not in st.session_state:
-        st.session_state.current_page = 'home'
-    # 渲染側邊欄
-    render_sidebar()
-    
-    # 頁面路由
-    if st.session_state.current_page == 'home':
-        render_home_page()
-    elif st.session_state.current_page == 'search':
-        render_search_page()
-    elif st.session_state.current_page == 'analysis':
+models = genai.list_models()
 
-     render_analysis_page()
-    elif st.session_state.current_page == 'compare':
-        render_compare_page()
-
-if __name__ == "__main__":
-    main()
+for m in models:
+    print(m.name)

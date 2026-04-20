f05a0e89b1809fa601f4ea8673a723001ccb7769
AeTen07 <w101882048@gmail.com>
2026-04-20 23:31:59 +0800

    Update main_test.py

--- main_test.py
+++ main_test.py
@@ -1,8 +1,8 @@
 import google.generativeai as genai
 
-genai.configure(api_key="AIzaSyAKE7etexSgsiI41vjHnXg3UcUVNJoSEhE")
+genai.configure(api_key="AIzaSyCeHCeUDsrOHP0Fziqyr-2vqH7GR1W73Bk")
 
 models = genai.list_models()
 
 for m in models:
-    print(m.name)
+    print(m.name, "->", m.supported_generation_methods)
